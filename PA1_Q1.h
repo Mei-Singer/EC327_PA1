71f51387a07a5aa09f4b771e5fc9a742b2c3b64e
Mei Singer <91206071+Mei-Singer@users.noreply.github.com>
2021-10-08 15:42:13 -0400

    Delete PA1_Q1.h

--- PA1_Q1.h
+++
@@ -1,8 +0,0 @@
-//Function prototypes
-double slope(double x1, double y1, double x2, double y2);
-double yIntercept (double x1, double y1, double m);
-double perpSlope(double m);
-double perpInt (double x3, double y3, double perpM);
-double intersectx (double m, double yInt, double perpM, double perpYInt);
-double intersecty (double m, double x, double yInt);
-double dist(double x, double y, double x3, double y3);
