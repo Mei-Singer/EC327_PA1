2d94721e9d841ab18f348101609b00f4c3acdc69
Mei Singer <91206071+Mei-Singer@users.noreply.github.com>
2021-10-08 16:57:41 -0400

    Add files via upload

---
+++ Q2_main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+using namespace std;
+
+//Function prototypes
+string compChoice(int n);
+string gameResult(int choice, int num);
+
+int main()
+{
+        //Prompt user to choose rock, paper, or scissors
+        int choice,n;
+        cout << "Choose Rock (0), Paper (1), or Scissors (2):" << endl;
+        cin >> choice;
+
+
+        //Generate a number between 0 and 2
+        srand(time(NULL));
+        n = rand() % 3;
+
+        //Invoke computer choice function
+        cout << "Computer chooses: ";
+        cout << compChoice(n) << endl;
+
+        //Invoke game function
+        cout << gameResult(choice,n) << endl;
+
+        return 0;
+}
+
+//Print the computer's choice
+string compChoice(int n)
+{
+        if (n == 0)
+        {
+                return "Rock";
+        }
+
+        else if (n == 1)
+        {
+                return "Paper";
+        }
+
+        else
+        {
+                return "Scissors";
+        }
+}
+
+//Game function and print results
+string gameResult(int choice, int n)
+{
+        if (choice == n)
+        {
+                return "You tie!";
+        }
+
+        else if ((choice == 0) && (n == 2))
+        {
+                return "You win!";
+        }
+
+        else if ((choice == 1) && (n == 0))
+        {       
+                return "You win!";
+        }
+        
+        else if ((choice == 2) && (n == 1))
+        {       
+                return "You win!";
+        }
+        
+        else
+        {       
+                return "You lose!";
+        }
+ 
+}
