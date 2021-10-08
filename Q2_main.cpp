50253cdf83b20d74a73f9eb19eca55d252076d5f
Mei Singer <91206071+Mei-Singer@users.noreply.github.com>
2021-10-08 16:50:25 -0400

    Update Q2_main.cpp

--- Q2_main.cpp
+++ Q2_main.cpp
@@ -32,3 +32,51 @@ int main()
 
         return 0;
 }
+//Print the computer's choice
+string compChoice(int n)
+{
+        if (n == 0)
+        {
+                return "Rock";
+        }
+
+        else if (n == 1)
+        {
+                return "Paper";
+        }
+
+        else
+        {
+                return "Scissors";
+        }
+}
+
+//Game function and print results
+string gameResult(int choice, int n)
+{
+        if (choice == n)
+        {
+                return "You tie!";
+        }
+
+        else if ((choice == 0) && (n == 2))
+        {
+                return "You win!";
+        }
+
+        else if ((choice == 1) && (n == 0))
+        {       
+                return "You win!";
+        }
+        
+        else if ((choice == 2) && (n == 1))
+        {       
+                return "You win!";
+        }
+        
+        else
+        {       
+                return "You lose!";
+        }
+ 
+}
