c84c347383661612ded89db0fcfcda05d508ab5b
Mei Singer <91206071+Mei-Singer@users.noreply.github.com>
2021-10-08 15:41:56 -0400

    Update PA1_Q1_main.cpp

--- PA1_Q1_main.cpp
+++ PA1_Q1_main.cpp
@@ -3,6 +3,15 @@
 #include "PA1_Q1.h"
 using namespace std;
 
+//Function prototypes
+double slope(double x1, double y1, double x2, double y2);
+double yIntercept (double x1, double y1, double m);
+double perpSlope(double m);
+double perpInt (double x3, double y3, double perpM);
+double intersectx (double m, double yInt, double perpM, double perpYInt);
+double intersecty (double m, double x, double yInt);
+double dist(double x, double y, double x3, double y3);
+
 int main()
 {
         //Prompt user to enter three pairs of x,y coordinates
