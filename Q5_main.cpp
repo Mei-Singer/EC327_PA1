98b3b245b7c23d1b5a5fb44f2d73718750adbf4c
Mei Singer <91206071+Mei-Singer@users.noreply.github.com>
2021-10-10 19:26:18 -0400

    Add files via upload

---
+++ Q5_main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+//Function prototypes
+int toASCII (char char1);
+char convert(int a, int offset);
+
+int main()
+{
+        char char1;
+        int offset;
+        cout << "Enter character: " ;
+        cin >> char1;
+        cout << "Offset (enter 0 to convert case): ";
+        cin >> offset;
+
+        //Convert character to ASCII
+        int a = int(char1);
+
+        //Add the offset and display new value
+        cout << "New character: " << convert (a, offset) << endl;
+
+        return 0;
+}
+
+//Apply offset
+char convert(int a, int offset)
+{
+        int b;
+        if (offset != 0)
+        {
+                if ((a > 47) && (a < 58))
+                {
+                        b = a;
+                }
+                else
+                {
+                        b = a + offset;
+                }
+        }
+
+        else
+        {
+                if ((a > 64) && (a < 91))
+                {
+                        b = a + 32;
+                }
+                else if ((a > 96) && (a < 123))
+                {
+                        b = a - 32;
+                }
+                else if ((a > 47) && (a < 58))
+                {
+                        b = a;
+                }
+                else
+                {
+                        return 0;
+                }
+        }
+        return b;
+}
